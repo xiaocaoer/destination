d25b4de269982652c84dbf924657e70a8179a37d
xiaocaoer <1608708253@qq.com>
2020-05-07 23:40:45 +0800

    首页轮播完事

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,30 +1,34 @@
 <template>
-  <div class="homeContainer">
-    <!-- 头部 -->
-    <div class="headerContainer">
-      <header>
-        <h1><img src="./logo.png" alt="logo"></h1>
-        <div class="search" >
-          <i class="iconfont icon-sousuo"></i>
-          <span>搜索商品,共25372款产品</span>
-        </div>
-        <button class="login">登录</button>
-      </header>
-      <!-- 上方导航 -->
-      <nav class="navContainer" ref="navContainer">
-        <ul class="navList">
-          <!-- 这里第一个是固定的推荐,然后是请求回来的那些数据的导航 -->
-          <li class="navItem" :class="{active:navIndex===0 }" @click="changeNav(0)">推荐</li>
-          <li class="navItem" :class="{active:navIndex===index+1}" @click="changeNav(index+1)" v-for="(navItem,index) in indexNav" :key="navItem.id">
-            {{navItem.name}}
-          </li>
-        </ul>
-      </nav>
+  <div class="home"  >
+    <div class="homeContainer">
+      <!-- 头部 -->
+      <div class="headerContainer">
+        <header>
+          <h1><img src="./logo.png" alt="logo"></h1>
+          <div class="search" >
+            <i class="iconfont icon-sousuo"></i>
+            <span>搜索商品,共25372款产品</span>
+          </div>
+          <button class="login">登录</button>
+        </header> 
+        <!-- 上方导航 -->
+          <nav class="navContainer" ref="navContainer">
+          <ul class="navList"> 
+            <!-- 这里第一个是固定的推荐,然后是请求回来的那些数据的导航 -->
+            <li class="navItem" :class="{active:navIndex===0 }" @click="changeNav(0)">推荐</li>
+            <li class="navItem" :class="{active:navIndex===index+1}" @click="changeNav(index+1)" v-for="(navItem,index) in indexNav" :key="navItem.id">
+              {{navItem.name}}
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <!-- <router-view></router-view> -->
+      <div class="contentContainer" ref="home">
+        <yan-recommend></yan-recommend>
+      </div>
     </div>
 
-    <!-- <router-view></router-view> -->
-    <yan-recommend></yan-recommend>
-   
   </div>
 </template>
 
@@ -64,90 +68,107 @@ export default {
               this.scroll = new BScroll(this.$refs.navContainer, {scrollX:true,click: true}) 
           }) 
       },
+      //整个页面滑屏
+      allScroll(){
+        this.$nextTick(() => { 
+          this.scroll = new BScroll(this.$refs.home) 
+        }) 
+      }
   },
   async mounted(){
     await this[GETINDEXDATA]();
     await this[GETINDEXNAV]();
     this.initScroll()
+    this.allScroll()
   }
 }
 </script>
 
 <style scoped lang="stylus">
-.homeContainer
+.home
   width 100%
   height 100%
-  .headerContainer
+  .homeContainer
     width 100%
-    position fixed
-    left 0
-    top 0
-    header 
+    overflow hidden //清除塌陷
+    .headerContainer
       width 100%
-      display flex
-      align-items center
-      padding 16px 30px
-      h1
-        height 40px
-        width 138px
-        margin-right 20px
-        img 
-          height 100%
-          width 100%
-      .search
-        width 422px
-        height 56px
-        line-height 56px
-        font-size 28px
-        border-radius 10px
-        color #666666
-        background-color #ededed
-        text-align center
-        .iconfont 
-          margin-right 10px
-      .login
-        width 72px
-        height 40px
-        line-height 38px
-        border 2px solid #DD1A21
-        font-size 24px
-        border-radius 5px
-        margin-left 16px
-        white-space  nowrap
-        color  #DD1A21
-    .navContainer
-      width 100%
-      .navList
+      height 150px
+      position fixed
+      z-index 99
+      background-color #fff
+      left 0
+      top 0
+      header 
+        width 100%
         display flex
-        flex-wrap nowrap
-        // 下面这两行,让ul不要继承父盒子的宽度
-        width -moz-fit-content
-        width fit-content
-        .navItem
-          position relative
-          height 60px
-          line-height 60px
+        align-items center
+        padding 16px 30px
+        h1
+          height 40px
+          width 138px
+          margin-right 20px
+          img 
+            height 100%
+            width 100%
+        .search
+          width 422px
+          height 56px
+          line-height 56px
           font-size 28px
-          padding 0 16px
-          color #333
-          white-space nowrap
-          &.active
-            color #DD1A21
-            &:after
-              position absolute
-              bottom 2px
-              left 10%
-              content ''
-              height 2px
-              width 80% 
-              background #DD1A21
-
+          border-radius 10px
+          color #666666
+          background-color #ededed
+          text-align center
+          .iconfont 
+            margin-right 10px
+        .login
+          width 72px
+          height 40px
+          line-height 38px
+          border 2px solid #DD1A21
+          font-size 24px
+          border-radius 5px
+          margin-left 16px
+          white-space  nowrap
+          color  #DD1A21
+      .navContainer
+        width 100%
+        .navList
+          display flex
+          flex-wrap nowrap
+          // 下面这两行,让ul不要继承父盒子的宽度
+          width -moz-fit-content
+          width fit-content
+          .navItem
+            position relative
+            height 60px
+            line-height 60px
+            font-size 28px
+            padding 0 16px
+            color #333
+            white-space nowrap
+            &.active
+              color #DD1A21
+              &:after
+                position absolute
+                bottom 2px
+                left 10%
+                content ''
+                height 2px
+                width 80% 
+                background #DD1A21
+    .contentContainer
+      margin-top 150px
+      height 81.4vh
 
               
 
           
 
    
+
+
 
 
 
--- src/pages/yan-recommend/yan-recommend.vue
+++ src/pages/yan-recommend/yan-recommend.vue
@@ -25,10 +25,98 @@
     <!-- 底部三个图标+文字小东西 -->
     <div class="policyContainer">
       <div class="policyDescList" v-for="(policyItem,index) in indexData.policyDescList" :key="index">
-        <image :src="policyItem.icon" mode=""></image>
-        <text>{{policyItem.desc}}</text>
+        <img :src="policyItem.icon" mode=""/>
+        <span>{{policyItem.desc}}</span>
       </div>
     </div>
+    <!-- 十个小个子 kingKongList-->
+    <div class="kingKongList" v-if="indexData.kingKongModule">
+      <div class="kingKongItem" v-for="(kingKongItem) in indexData.kingKongModule.kingKongList" :key="kingKongItem.L1Id">
+        <img :src="kingKongItem.picUrl" alt="">
+        <span>{{kingKongItem.text}}</span>
+      </div>
+    </div>
+    <!-- 新用户专享口罩图 -->
+    <div class="newUser">
+      <img src="https://yanxuan.nosdn.127.net/7db5ad89be165b82b6a3e261029d0122.gif?imageView&quality=75" alt="">
+    </div>
+    <!-- 新人专享礼 -->
+    <div class="gift">
+      <div class="text">— 新人专享礼 —</div>
+      <div class="left">
+        <span>新人专享红包</span>
+        <img src="" alt="" class="redPack">
+      </div>
+      <div class="right">
+        <div class="rTop"></div>
+        <div class="rbottom"></div>
+      </div>
+    </div>
+
+    <!-- //占位 -->
+        <div class="swiper-container">
+      <ul class="swiper-wrapper">
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/3c6358daba8bca7169b0dd2a49df188c.jpg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/f00ff1a6f9e244efe43c77ee8331318f.jpg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/8c06fc58e849da6cbaf2838d27f072f3.jpeg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/5e658f72294572822b65e09113ac4311.jpg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/d8645b184bb0b7c9e471d9212d283939.jpg" alt />
+        </li>
+      </ul>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+    </div>    <div class="swiper-container">
+      <ul class="swiper-wrapper">
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/3c6358daba8bca7169b0dd2a49df188c.jpg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/f00ff1a6f9e244efe43c77ee8331318f.jpg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/8c06fc58e849da6cbaf2838d27f072f3.jpeg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/5e658f72294572822b65e09113ac4311.jpg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/d8645b184bb0b7c9e471d9212d283939.jpg" alt />
+        </li>
+      </ul>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+    </div>    <div class="swiper-container">
+      <ul class="swiper-wrapper">
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/3c6358daba8bca7169b0dd2a49df188c.jpg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/f00ff1a6f9e244efe43c77ee8331318f.jpg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/8c06fc58e849da6cbaf2838d27f072f3.jpeg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/5e658f72294572822b65e09113ac4311.jpg" alt />
+        </li>
+        <li class="swiper-slide">
+          <img src="https://yanxuan.nosdn.127.net/d8645b184bb0b7c9e471d9212d283939.jpg" alt />
+        </li>
+      </ul>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+    </div>
+
+    
   </div>
 </template>
 
@@ -37,9 +125,17 @@
 import Swiper from "swiper";
 import "swiper/css/swiper.min.css";
 import {mapState} from 'vuex'
+import Vue from 'vue';
+import { Grid, GridItem } from 'vant';
+
+Vue.use(Grid);
+Vue.use(GridItem);
 
 export default {
   name: "yan-recommed",
+  computed:{
+    ...mapState(['indexData'])
+  },
   methods: {
     //轮播图
     renderSwiper() {
@@ -63,9 +159,6 @@ export default {
 <style scoped lang="stylus">
 .commendContainer
   .swiper-container
-    position relative
-    top 150px
-    left 0
     .swiper-wrapper
       display flex
       .swiper-slide
@@ -73,4 +166,63 @@ export default {
         img 
           width 100%
           height 296px
+  .policyContainer
+    display flex
+    align-items center
+    height 72px
+    width 100%
+    .policyDescList
+      text-align center
+      height 72px
+      width 100%
+      color #333
+      line-height 72px
+      img 
+        width 32px
+        height 32px
+        vertical-align middle
+        margin-right 5px
+      span 
+        font-size 24px
+        vertical-align middle
+  .kingKongList
+    display flex
+    flex-wrap wrap
+    margin 20px 0
+    .kingKongItem
+      width 20%
+      display flex
+      flex-direction column
+      align-items center
+      img
+        width 110px
+        height 110px
+      span
+        font-size 24px
+        line-height 50px
+  .newUser
+    width 100%
+    height 240px
+    img 
+      width 100%
+      height 100%
+  .gift
+    width 100%
+    height 556px
+    .text
+      padding 0 30px
+      background #ffffff
+      color #333
+      font-size 32px
+      height 90px
+      line-height 90px
+      text-align center
+    .left
+      width 343px
+      height 434px
+      span
+        padding 30px 0 0 30px
+        font-size 32px
+        color #333
+      
 </style>
